96218b03f099c4ea0cb0e485ced0422ac29ddfc7
Muhammad Zafar <xafarr@gmail.com>
2024-02-16 16:38:11 +1100

    Use spaces instead of tabs for indentation

--- scripts/clima.sh
+++ scripts/clima.sh
@@ -12,44 +12,44 @@ SHOW_ICON=$(get_tmux_option @clima_show_icon 1)
 SHOW_LOCATION=$(get_tmux_option @clima_show_location 1)
 
 clima() {
-	NOW=$(date +%s)
-	LAST_UPDATE_TIME=$(get_tmux_option "@clima_last_update_time")
-	MOD=$((NOW - LAST_UPDATE_TIME))
-	SYMBOL=$(symbol "$UNIT")
-	if [ -z "$LAST_UPDATE_TIME" ] || [ "$MOD" -ge "$TTL" ]; then
-		LOCATION=$(curl --silent https://ifconfig.co/json)
-		LAT=$(echo "$LOCATION" | jq .latitude)
-		LON=$(echo "$LOCATION" | jq .longitude)
-		WEATHER=$(curl --silent http://api.openweathermap.org/data/2.5/weather\?lat="$LAT"\&lon="$LON"\&APPID="$OPEN_WEATHER_API_KEY"\&units="$UNIT")
-		if [ "$?" -eq 0 ]; then
-			CATEGORY=$(echo "$WEATHER" | jq .weather[0].id)
-			TEMP="$(echo "$WEATHER" | jq .main.temp | cut -d . -f 1)$SYMBOL"
-			ICON="$(icon "$CATEGORY")"
-			CITY="$(echo "$LOCATION" | jq -r .city)"
-			COUNTRY="$(echo "$LOCATION" | jq -r .country)"
-			DESCRIPTION="$(echo "$WEATHER" | jq -r .weather[0].main)"
-			FEELS_LIKE="Feels like: $(echo "$WEATHER" | jq .main.feels_like | cut -d . -f 1)$SYMBOL"
-			WIND_SPEED="Wind speed: $(echo "$WEATHER" | jq .wind.speed) m/s"
-			CLIMA=""
+    NOW=$(date +%s)
+    LAST_UPDATE_TIME=$(get_tmux_option "@clima_last_update_time")
+    MOD=$((NOW - LAST_UPDATE_TIME))
+    SYMBOL=$(symbol "$UNIT")
+    if [ -z "$LAST_UPDATE_TIME" ] || [ "$MOD" -ge "$TTL" ]; then
+        LOCATION=$(curl --silent https://ifconfig.co/json)
+        LAT=$(echo "$LOCATION" | jq .latitude)
+        LON=$(echo "$LOCATION" | jq .longitude)
+        WEATHER=$(curl --silent http://api.openweathermap.org/data/2.5/weather\?lat="$LAT"\&lon="$LON"\&APPID="$OPEN_WEATHER_API_KEY"\&units="$UNIT")
+        if [ "$?" -eq 0 ]; then
+            CATEGORY=$(echo "$WEATHER" | jq .weather[0].id)
+            TEMP="$(echo "$WEATHER" | jq .main.temp | cut -d . -f 1)$SYMBOL"
+            ICON="$(icon "$CATEGORY")"
+            CITY="$(echo "$LOCATION" | jq -r .city)"
+            COUNTRY="$(echo "$LOCATION" | jq -r .country)"
+            DESCRIPTION="$(echo "$WEATHER" | jq -r .weather[0].main)"
+            FEELS_LIKE="Feels like: $(echo "$WEATHER" | jq .main.feels_like | cut -d . -f 1)$SYMBOL"
+            WIND_SPEED="Wind speed: $(echo "$WEATHER" | jq .wind.speed) m/s"
+            CLIMA=""
 
-			if [ "$SHOW_LOCATION" == 1 ]; then
-				CLIMA="$CLIMA$CITY, "
-			fi
+            if [ "$SHOW_LOCATION" == 1 ]; then
+                CLIMA="$CLIMA$CITY, "
+            fi
 
-			if [ "$SHOW_ICON" == 1 ]; then
-				CLIMA="$CLIMA$ICON"
-			fi
+            if [ "$SHOW_ICON" == 1 ]; then
+                CLIMA="$CLIMA$ICON"
+            fi
 
-			CLIMA="$CLIMA $TEMP"
-			CLIMA_DETAILS="${CITY}, ${COUNTRY}: ${ICON} ${TEMP}, ${DESCRIPTION}, ${FEELS_LIKE}, ${WIND_SPEED}"
+            CLIMA="$CLIMA $TEMP"
+            CLIMA_DETAILS="${CITY}, ${COUNTRY}: ${ICON} ${TEMP}, ${DESCRIPTION}, ${FEELS_LIKE}, ${WIND_SPEED}"
 
-			set_tmux_option "@clima_last_update_time" "$NOW"
-			set_tmux_option "@clima_current_value" "$CLIMA"
-			set_tmux_option "@clima_details_value" "$CLIMA_DETAILS"
-		fi
-	fi
+            set_tmux_option "@clima_last_update_time" "$NOW"
+            set_tmux_option "@clima_current_value" "$CLIMA"
+            set_tmux_option "@clima_details_value" "$CLIMA_DETAILS"
+        fi
+    fi
 
-	echo -n "$(get_tmux_option "@clima_current_value")"
+    echo -n "$(get_tmux_option "@clima_current_value")"
 }
 
 clima
--- scripts/icons.sh
+++ scripts/icons.sh
@@ -3,76 +3,76 @@
 NERD_FONT=$(get_tmux_option @clima_use_nerd_font 0)
 
 symbol() {
-	case $1 in
-	metric)
-		echo '℃'
-		;;
-	imperial)
-		echo '℉'
-		;;
-	kelvin)
-		echo ' K'
-		;;
-	*)
-		echo '℃'
-		;;
-	esac
+    case $1 in
+        metric)
+            echo '℃'
+            ;;
+        imperial)
+            echo '℉'
+            ;;
+        kelvin)
+            echo ' K'
+            ;;
+        *)
+            echo '℃'
+            ;;
+    esac
 }
 
 icon() {
-	case $1 in
-	# Thunderstorm group
-	200 | 201 | 202 | 210 | 211 | 212 | 221 | 230 | 231 | 232)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌩 '
-		;;
-		# Drizzle group
-	300 | 301 | 302 | 310 | 311 | 312 | 313 | 314 | 321)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌧 '
+    case $1 in
+            # Thunderstorm group
+        200 | 201 | 202 | 210 | 211 | 212 | 221 | 230 | 231 | 232)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌩 '
+            ;;
+            # Drizzle group
+        300 | 301 | 302 | 310 | 311 | 312 | 313 | 314 | 321)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌧 '
 
-		;;
-		# Rain group
-	500 | 501 | 502 | 503 | 504)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌦 '
-		;;
-	511)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '❄ '
-		;;
-	520 | 521 | 522 | 531)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌧'
-		;;
-		# Snow group
-	600 | 601 | 602)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '❄ '
-		;;
-	611 | 612 | 613 | 615 | 616 | 620 | 621 | 622)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌨 '
-		;;
-		# Atmosphere group
-	701 | 711 | 721 | 731 | 751 | 761 | 762 | 771)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '  '
-		;;
-	741)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌫 '
-		;;
-	781)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌪 '
-		;;
-		# Clear group
-	800)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '☼ '
-		;;
-		# Clouds group
-	801)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌤 '
-		;;
-	802 | 804)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '☁ '
-		;;
-	803)
-		[[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌥 '
-		;;
-	*)
-		echo "$1"
-		;;
-	esac
+            ;;
+            # Rain group
+        500 | 501 | 502 | 503 | 504)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌦 '
+            ;;
+        511)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '❄ '
+            ;;
+        520 | 521 | 522 | 531)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌧'
+            ;;
+            # Snow group
+        600 | 601 | 602)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '❄ '
+            ;;
+        611 | 612 | 613 | 615 | 616 | 620 | 621 | 622)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌨 '
+            ;;
+            # Atmosphere group
+        701 | 711 | 721 | 731 | 751 | 761 | 762 | 771)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '  '
+            ;;
+        741)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌫 '
+            ;;
+        781)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌪 '
+            ;;
+            # Clear group
+        800)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '☼ '
+            ;;
+            # Clouds group
+        801)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌤 '
+            ;;
+        802 | 804)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '☁ '
+            ;;
+        803)
+            [[ $NERD_FONT == 1 ]] && echo ' ' || echo '🌥 '
+            ;;
+        *)
+            echo "$1"
+            ;;
+    esac
 }
